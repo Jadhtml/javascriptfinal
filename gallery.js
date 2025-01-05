09ed77c4e2f6062550c7332b843ee43a9d75fec0
Jadhtml <jathushan20@gmail.com>
2025-01-05 17:58:01 -0500

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,19 @@
+                
+
+/*Name this external file gallery.js*/
+function upDate(previewPic) {
+
+    document.getElementById('image').innerHTML = previewPic.alt;
+    document.getElementById('image').style.backgroundImage = "url(" + previewPic.src + ")";
+}
+
+function unDo() {
+
+    document.getElementById('image').style.backgroundImage = "url('')"
+    document.getElementById('image').innerHTML = 'Hover over an image below to display here.';
+}
+
+previewImages.forEach((img) => {
+    img.addEventListener('focus', () => upDate(img)); 
+    img.addEventListener('blur', unDo); 
+});
